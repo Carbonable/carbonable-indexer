61d330ce920a60e92f6412c9c74800f16e4c81eb
bal7hazar <bal7hazar@proton.me>
2023-01-26 21:37:14 +0100

    🗑️ reset database

--- prisma/migrations/20230125131659_init/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `Whitelist` on the `Minter` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Minter" DROP COLUMN "Whitelist",
-ADD COLUMN     "whitelist" JSONB;
